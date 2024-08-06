3405f9e47a300a3e265f3331ad02a9fe5cb5136a
pradeep-heu-ai <pradeep.biswas@heu.ai>
2024-08-06 20:03:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Donations } from "./components/Donations"
+import FlowDataOfDonation from "./components/FlowDataOfDonation"
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* <Donations /> */}
+      <FlowDataOfDonation />
+    </>
+  )
 }
-
-export default App;
---
+++ src/components/Donations.jsx
@@ -0,0 +1,98 @@
+import { Button, Card, CardContent, CardHeader, CircularProgress, Grid } from "@mui/material"
+import { Field, FieldArray, Form, Formik } from "formik"
+import { CheckboxWithLabel, TextField } from "formik-material-ui"
+import { object, number, string, boolean, array } from "yup"
+
+export const Donations = () => {
+  return (<>
+    <div style={{ margin: '15px' }}>
+      <Card>
+        <CardContent>
+          <Formik
+            initialValues={{
+              fullName: '',
+              donationsAmount: 0,
+              termsAndConditions: false,
+              donations: [{ institution: '', percentage: 0 }]
+            }}
+            validationSchema={object({
+              fullName: string().required('FullName is requried').min(2, 'FullName should be atleast more than 2 character').max(10, 'FullName should be less than 10 character'),
+              donationsAmount: number().required('Donation is required').min(10, 'Donation should be more than 10'),
+              termsAndConditions: boolean().required().isTrue(),
+              donations: array(object({
+                institution: string().required('Institutaion Name is required').min(3, 'Institution should be more than 3').max(10, 'Institution should be less than 10'),
+                percentage: number().required('Percentage is required').min(1, 'Percentage should be more than 1').max(10, 'Percentage should be less than 10')
+              })).min(1).max(3)
+            })}
+            onSubmit={async (values, { resetForm }) => {
+              await new Promise(res => setTimeout(res, 2400))
+              resetForm()
+            }}
+          >
+            {({ values, errors, isSubmitting }) => (
+              <Form>
+                <Grid container direction={'column'} spacing={2}>
+                  <Grid item>
+                    <Field fullWidth name='fullName' type='text' component={TextField} label='First Name' />
+                  </Grid>
+                  <Grid item>
+                    <Field fullWidth name='donationsAmount' type='number' component={TextField} label='Donation' />
+                  </Grid>
+
+                  <div>
+                    <FieldArray name="donations">
+                      {({ push, remove }) => (
+                        <>
+                          <Card>
+                            <CardHeader
+                              title="All Donations here"
+                            />
+                            <CardContent>
+                              {values.donations.map((_, index) =>
+                                <Grid container item spacing={2} marginTop={1}>
+                                  <Grid item>
+                                    <Field name={`donations[${index}].institution`} component={TextField} label='Institution' />
+                                  </Grid>
+                                  <Grid item>
+                                    <Field name={`donations[${index}].percentage`} type='number' component={TextField} label='Percentage' />
+                                  </Grid>
+                                  <Grid item>
+                                    <Button onClick={() => remove(index)}>Delete</Button>
+                                  </Grid>
+                                </Grid>
+                              )}
+                              {
+                                typeof errors.donations === 'string' ? <><p style={{ color: 'red' }}>{errors.donations}</p></> : null
+                              }
+                              <Grid item marginTop={1}>
+                                <Button onClick={() => push({ institution: '', percentage: 0 })}>Add</Button>
+                              </Grid>
+                            </CardContent>
+                          </Card>
+                        </>
+                      )}
+                    </FieldArray>
+                  </div>
+                  <Grid item>
+                    <Field name='termsAndConditions' type='checkbox' component={CheckboxWithLabel} Label={{ label: 'I accept the terms and conditions' }} />
+                  </Grid>
+                  <Grid item>
+                    <Button disabled={isSubmitting}
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      startIcon={isSubmitting ? <CircularProgress size={'0.9rem'} /> : null}
+                    >
+                      {isSubmitting ? 'Submitting' : 'Submit'}
+                    </Button>
+                  </Grid>
+                </Grid>
+                <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+              </Form>
+            )}
+          </Formik>
+        </CardContent>
+      </Card>
+    </div >
+  </>)
+}
